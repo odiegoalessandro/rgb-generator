d7084616390dfa2840094914f98e62418f061179
Diego Alessandro <diegoalessandrodacruzmartins@gmail.com>
2021-12-01 20:53:02 -0300

    Initial commit from Create Next App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from "next/app"
+import { AnimatePresence } from "framer-motion"
+import { ChakraProvider } from "@chakra-ui/react"
+import theme from "../lib/chakra-theme"
+import GlobalStyles from "../styles/GlobalStyles"
+import Main from "../components/main"
+
+function MyApp({ Component, pageProps, router }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <AnimatePresence exitBeforeEnter initial={true}>
+        <Main>
+          <GlobalStyles />
+          <Component {...pageProps} key={router.route} />
+        </Main>
+      </AnimatePresence>
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
---
+++ src/pages/_document.tsx
@@ -0,0 +1,32 @@
+import Document, { Html, Head, Main, NextScript } from "next/document"
+import { ColorModeScript } from "@chakra-ui/react"
+import React from "react"
+import Fonts from "../components/fonts"
+import theme from "../lib/chakra-theme"
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <>
+        <Html lang="pt-BR">
+          <Head>
+            <link rel="icon" type="image/png" href="image_url" />
+            <Fonts />
+          </Head>
+          <body>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <Main />
+            <NextScript />
+          </body>
+        </Html>
+      </>
+    )
+  }
+}
+
+export default MyDocument
